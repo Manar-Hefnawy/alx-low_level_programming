285155c933a59055282209758ce0ea04c1ad508c
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-05 14:05:49 -0700

    Create 4-puts.c source file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * put - prints stream of characters
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
