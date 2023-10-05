c5da1070ed5e1ef9fcf88c13af4b37cf421cf51c
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-05 14:18:44 -0700

    Modifying 5-printf.c source code file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * prinf()- prints the inputed string formatted
+ * Description: a C program that uses  prinf() function to  print the inputed string formatted
  * return: always 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
