67399d7a4c907bc7536b5b8f27452c8d9bfc10ac
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-06 08:47:15 -0700

    Create 100-print_comb4.c file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C program that prints all possible combinations
+ * of single digit numbers,
+ * separated by ,, followed by a space,
+ * followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int d;
+
+	for (i = 0; i <= 8; ++i)
+	{
+		for ( d = i + 1; d <= 9; ++d)
+		{
+			putchar('0' + i);
+			putchar('0' + d);
+
+			if (i ==8 && d ==9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
