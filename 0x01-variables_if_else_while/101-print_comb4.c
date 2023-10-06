c84ad2f98a51f28e350870e166cb3420fcda1141
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-06 09:02:11 -0700

    Create 101-print_comb4.c file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,11 +15,11 @@ int main(void)
 	int d2;
 	int d3;
 
-	for (d1 = 0; d1 <= 7; ++d1)
+	for (d1 = 0; d1 < 8; ++d1)
 	{
-		for (d2 = d1 + 1; d2 <= 8; ++d2)
+		for (d2 = d1 + 1; d2 < 9; ++d2)
 		{
-			for (d3 = d2 + 1; d2 <= 9; ++d3)
+			for (d3 = d2 + 1; d2 < 10; ++d3)
 			{
 				putchar('0' + d1);
 				putchar('0' + d2);
