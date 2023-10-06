ba1949a520b8c6fb36f7c00efbb0d8f15b7ccedb
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-06 14:34:15 -0700

    Modify 102-print_comb5.c source file styling

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,22 +5,25 @@
  *
  * Description: C program that prints all possible combinations of
  * two two-digit numbers.
+ *
+ * Return: 0
  */
 
 int main(void)
 {
 	int i, d;
+
 	for (i = 0; i < 100; i++)
 	{
 		for (d = 0; d < 100; d++)
 		{
 			if (i < d)
 			{
-				putchar(( i / 10) + '0');
-				putchar(( i % 10) + '0');
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar(( d / 10) + '0');
-				putchar(( d % 10) + '0');
+				putchar((d / 10) + '0');
+				putchar((d % 10) + '0');
 
 				if (i != 98 || d != 99)
 				{
@@ -32,5 +35,5 @@ int main(void)
 	}
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
