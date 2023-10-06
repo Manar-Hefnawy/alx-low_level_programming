8f8d9e7cd3a5598a17fc8f30661e5d97e51effb4
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-06 06:26:48 -0700

    Create 2-print_alphabet.c source code file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C program that prints the alphabet in lowercase,
+ * followed by a new line.
+ * Return: 0
+ */
+
+int main()
+{
+	char c;
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return 0;
+}
