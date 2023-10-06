49baa551e35ec72890aba116885c532b101d5d78
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-06 05:48:38 -0700

    Create 1-last_digit.c source code file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C program that assign a random number to the variable n
+ * each time it is executed and print the last digit of the number
+ * stored in the variable n.
+ * Return: 0
+ * */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int lastdigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastdigit = n%10
+	if (lastdigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+	} else if (lastdigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+	} else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+	}
+	return (0);
+}
