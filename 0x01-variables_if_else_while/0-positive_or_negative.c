65a28589b09b51ff68bedf553381e6346966f70e
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-06 05:14:35 -0700

    Modify 0-positive_or_negative.c source code file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,8 @@
 /**
  * main - Entry point
  *
- * Description: C program that assign a random number to the variable n 
- * each time it is executed and print whether the number stored 
+ * Description: C program that assign a random number to the variable n
+ * each time it is executed and print whether the number stored
  * in the variable n is positive, negative or zero.
  *
  * Return: 0
@@ -18,12 +18,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0){
-	    printf("%d is positive\n", n);
-	}else if(n<0){
-	    printf("%d is negative\n", n);
-	}else{
-	    printf("%d is zero\n", n);
+	if(n>0)
+	{
+		printf("%d is positive\n", n);
+	}else if(n<0)
+	{
+		printf("%d is negative\n", n);
+	}else
+	{
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
