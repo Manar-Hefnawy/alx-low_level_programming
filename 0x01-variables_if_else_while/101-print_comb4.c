b544806c1528931d080f704675bacde60fdb31fd
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-06 09:12:55 -0700

    Create 101-print_comb4.c file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: C program that prints all possible combinations
- * of single digit numbers,
- * separated by ,, followed by a space,
- * followed by a new line.
- * Return: 0
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int d1;
-	int d2;
-	int d3;
+	int d1, d2, d3;
 
 	for (d1 = 0; d1 < 8; d1++)
 	{
 		for (d2 = d1 + 1; d2 < 9; d2++)
 		{
-			for (d3 = d2 + 1; d2 < 10; d3++)
+			for (d3 = d2 + 1; d3 < 10; d3++)
 			{
-				putchar(d1 + '0');
-				putchar(d2 + '0');
-				putchar(d3 + '0');
+				putchar((d1 % 10) + '0');
+				putchar((d2 % 10) + '0');
+				putchar((d3 % 10) + '0');
 
 				if (d1 == 7 && d2 == 8 && d3 == 9)
 					continue;
@@ -32,6 +27,7 @@ int main(void)
 			}
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
