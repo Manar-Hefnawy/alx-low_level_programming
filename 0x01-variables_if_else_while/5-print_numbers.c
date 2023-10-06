2ffedee60771df519bdf6063b2f47f9fe228c617
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-06 07:01:47 -0700

    Create 5-print_numbers source code file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Description: C program that prints all single digit numbers of base 10 
+ * Description: C program that prints all single digit numbers of base 10
  * starting from 0, followed by a new line.
  * Return: 0
  */
