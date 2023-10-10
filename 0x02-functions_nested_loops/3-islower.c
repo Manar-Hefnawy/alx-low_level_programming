7b7256e8a19f915558d6bb664412c213058147f8
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-10 16:43:32 -0700

    Create 3-islower.c source file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int_islower - function that returns 1 if the character 
+ * passed is lowercase and 0 if it's not
+ */
+
+int _islower(int c)
+{
+    if ((c >= 'a') && (c <= 'z'))
+    {
+        _putchar(49);
+    } else
+    {
+        _putchar(48);
+    }
+}
+
