a92e2c8f5639e46da526689e1f0633d7ae0695c0
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-10 15:33:57 -0700

    add function description to 1-alphabet.c source file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,14 @@
 #include "main.h"
-
-
-
-
+/**
+ * Description: C program that prints the alphabet,
+ * in lowercase, followed by a new line.
+ * Prototype: void print_alphabet(void);
+ * You can only use _putchar twice in your code
+ */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; ++c)
 	{
 		_putchar(c);
