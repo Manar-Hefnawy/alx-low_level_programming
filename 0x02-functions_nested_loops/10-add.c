89e597eb3d570d72dc906cc1184766a7d5274f2a
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-11 06:45:37 -0700

    Modify 10-add.c source code file styling

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,13 +3,13 @@
 /**
  * add - function the takes 2 integers and return their sum
  *
- * @int i: first input number
- * @int j: second input number
+ * @i: first input number
+ * @j: second input number
  *
  * Return: sum of i and j
  */
 
 int add(int i, int j)
 {
-	return(i + j);
+	return (i + j);
 }
