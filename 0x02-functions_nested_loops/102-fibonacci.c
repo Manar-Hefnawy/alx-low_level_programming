bd7a74e46fb52cb53d751de82f1adfa78f1578a4
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-11 13:27:14 -0700

    Create 102-fibonacci.c source code file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ * followed by a new line.
+ * The numbers must be separated by comma, followed by a space ,
+ *
+ * Return: 0 success
+ */
+
+unsigned int main()
+{
+	int i = 1, j = 2, t = 0, sum;
+
+	for (; t < 50; t++)
+	{
+		sum = i + j;
+		printf("%u", sum);
+		if (t == 49)
+			continue;
+		printf(", ");
+		i = j;
+		j = sum;
+	}
+	printf("\n");
+}
