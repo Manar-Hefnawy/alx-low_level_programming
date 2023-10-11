ab5531f3c3c8f6e87c4c7d27a9a86f53c1763d1f
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-11 08:46:15 -0700

    Create 9-times_table.c source code file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - a function that prints the 9 times table starting from 0
+ */
+
+void times_table(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = 0; j <= 9; j++)
+		{
+			if ((i * j) > 9)
+			{
+				_putchar((i * j / 10) + 48);
+				_putchar((i * j % 10) + 48);
+			} else
+			{
+				_putchar((i * j) + 48);
+			}
+			if (j == 9)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
