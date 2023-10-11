4d6f922c71da17187cfca4ca8514416a200537be
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-11 06:42:15 -0700

    Create 10-add.c source code file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function the takes 2 integers and return their sum
+ *
+ * @int i: first input number
+ * @int j: second input number
+ *
+ * Return: sum of i and j
+ */
+
+int add(int i, int j)
+{
+	return(i + j);
+}
