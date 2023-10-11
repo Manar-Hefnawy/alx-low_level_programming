f6d3598127695cce627afd7e2d866f06def89288
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-11 16:13:37 -0700

    fixing function output

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,23 +5,18 @@
  * in the Fibonacci sequence whose values do not exceed 4,000,000
  * followed by a new line.
  *
- * Retturn: 0 success
+ * Return: 0 success
  */
 
 int main(void)
 {
 	unsigned long i = 0, j = 2, sum;
-	printf("%lu, ", i);
-	printf("%lu, ", j);
+
 	for (; j <= 4000000; j = sum)
 	{
 		sum = i + j;
-		printf("%lu", sum);
-		if (sum >= 4000000)
-			continue;
-		printf(", ");
 		i = j;
 	}
-	printf("\n");
+	printf("%lu\n", sum);
 	return (0);
 }
