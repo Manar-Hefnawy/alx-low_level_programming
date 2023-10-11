c5995c2ef6d3319ea49f48c37bbb014750e3cb40
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-11 12:04:49 -0700

    Fixing code style

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,33 +9,37 @@
 void print_times_table(int n)
 {
 	int i, j, prod;
+
 	for (i = 0; i <= n; i++)
 	{
-		if (n < 0 || n > 15)
-			break;
-		else{
+		if (n >= 0 && n <= 15)
+		{
 			_putchar(48);
-			for (j = 1; j <= n; j++)
-			{
-				_putchar(',');
-				_putchar(' ');
-				prod = i * j;
-				if (prod <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				} else if (prod <= 99)
-				{
-					_putchar(' ');
-					_putchar((prod / 10) + 48);
-				}else
-				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod / 10 % 10) + 48);
-				}
-				_putchar((prod % 10) + 48);
-			}
-			_putchar('\n');
+                        for (j = 1; j <= n; j++)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                                prod = i * j;
+                                if (prod <= 9)
+                                {
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                } else if (prod <= 99)
+                                {
+                                        _putchar(' ');
+                                        _putchar((prod / 10) + 48);
+                                }else
+                                {
+                                        _putchar((prod / 100) + 48);
+                                        _putchar((prod / 10 % 10) + 48);
+                                }
+                                _putchar((prod % 10) + 48);
+                        }
+                        _putchar('\n');
+		}
+		else
+		{
+			break;
 		}
 	}
 }
