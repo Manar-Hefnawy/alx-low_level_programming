bb71f32124d9ec645ee7e214a70af3046dbe976f
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-10 17:06:55 -0700

    Modify 3-islower style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
- * int_islower - function that returns 1 if the character 
+ * _islower - function that returns 1 if the character
  * passed is lowercase and 0 if it's not
+ *
+ * @c : input of function
+ *
+ * Return: 1 if c is lower case and 0 if it's not
  */
 
 int _islower(int c)
 {
-    if (c >= 'a' && c <= 'z')
-	    return (1);
-    return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
 }
