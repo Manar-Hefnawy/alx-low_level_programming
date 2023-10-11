da26a8fdfd16d48cd4de2478900cb880207659a3
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-10 20:10:29 -0700

    Modify 8-24_hours.c style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,19 +9,19 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 	
-	while(hours <= 23)
+	while (hours <= 23)
 	{
-		while(minutes <= 59)
+		while (minutes <= 59)
 		{
 			_putchar((hours/10) + 48);
 			_putchar((hours%10) + 48);
-            		_putchar(':');
-            		_putchar((minutes/10) + 48);
-            		_putchar((minutes%10) + 48);
-            		_putchar('\n');
-            		minutes++;
-        	}
-         	hours++;
-        	minutes = 0;
-    	}
+			_putchar(':');
+			_putchar((minutes/10) + 48);
+			_putchar((minutes%10) + 48);
+			_putchar('\n');
+			minutes++;
+		}
+		hours++;
+		minutes = 0;
+	}
 }
