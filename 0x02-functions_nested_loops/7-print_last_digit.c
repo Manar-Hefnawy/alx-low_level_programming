7ddd29cf45fee3bc6acc38a6437125e70ba28522
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-10 18:53:09 -0700

    Create 7-print_last_digit.c file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of inputed number
+ *
+ * @n: input number
+ *
+ * Return: 0 always success
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	_putchar(last_digit + '0');
+	
+	return (0);
+}
