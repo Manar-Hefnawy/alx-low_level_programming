c1bfbae150b7ba655a8a0cd07453b954c35fe34a
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-10 19:24:14 -0700

    fixing 7-print_last_digit.c output

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,10 @@
 
 int print_last_digit(int n)
 {
-	int last_digit;
+	int last_digit = n % 10;
 
 	if (n < 0)
-		n = -1 * n;
-
-	last_digit = n % 10;
+		last_digit *= -1;
 
 	_putchar(last_digit + '0');
 	return (last_digit);
