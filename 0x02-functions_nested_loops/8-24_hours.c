d6af84979cc37fdeac1ef56ffb9e09d11eec82bf
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-10 20:00:49 -0700

    Create 8-24_hours.c source file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.c"
+
+/**
+ * jack_bauer - prints every minute of the day of jack bauer
+ */
+
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	
+	while(hours <= 23)
+	{
+		while(minutes <= 59)
+		{
+			_putchar((hours/10) + 48);
+			_putchar((hours%10) + 48);
+            		_putchar(':');
+            		_putchar((minutes/10) + 48);
+            		_putchar((minutes%10) + 48);
+            		_putchar('\n');
+            		minutes++;
+        	}
+         	hours++;
+        	minutes = 0;
+    	}
+}
