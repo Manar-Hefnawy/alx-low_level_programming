1c40b3dcf5f52069df9f9ba7776e325dc8924aef
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-11 21:41:49 -0700

    Create 3-print_numbers.c source code file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9 followed by new line
+ */
+
+void print_numbers(void)
+{
+	char i = 48;
+
+	for (; i <= 57; ++i)
+		_putchar(i);
+
+	_putchar('\n');
+}
