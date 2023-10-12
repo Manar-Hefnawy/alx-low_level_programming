67c8c1e826eb3afc74276db07b65eb5fb6579351
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-11 21:21:19 -0700

    Create 2-mul source code file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function the takes 2 integers and return their product
+ *
+ * @a: first input number
+ * @b: second input number
+ *
+ * Return: a multiplied by b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
