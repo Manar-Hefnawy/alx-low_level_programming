bafb6cbcdef21c265d9071ce26786b112045545a
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-12 03:41:47 -0700

    Modify function description

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0 to 9 except 2 & 4 followed by new line
+ * print_most_numbers - print numbers from 0 to 9 except 2 & 4 followed by new line
  */
 
 void print_most_numbers(void)
