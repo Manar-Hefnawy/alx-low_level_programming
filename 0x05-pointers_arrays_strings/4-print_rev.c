c1c614fceb5c4a8f4511d5676c77e3b03e95091c
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-19 14:51:12 -0700

    Create 4-print_rev.c source code file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line.
+ *
+ * @str: input string
+ */
+
+void print_rev(char *s)
+{
+	char *ptr = s, *i;
+
+	while (*ptr)
+		ptr++;
+
+	for (i = ptr - 1; i >= s; i--)
+		_putchar(*i);
+
+	_putchar('\n');
+}
