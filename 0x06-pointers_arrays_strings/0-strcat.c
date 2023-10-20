87d8a13fa870992ba5d33920e00e9bbb86bb60ec
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-19 17:50:44 -0700

    Create 0-strcat.c source code file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - a function that concatenates two strings.
+ *
+ * @dest: first string
+ * @src: second string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *cs = dest;
+
+	while (*dest != '\0')
+		dest++;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (cs);
+}
