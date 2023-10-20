dc1fd0ebd84e99042757d1402525ebb8320e35d7
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-19 21:37:40 -0700

    Create 5-string_toupper.c source code file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes all lowercase letters of
+ * a string to uppercase.
+ *
+ * @s: input string
+ *
+ * Return: Uppercase letter string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] >= 72 && s[i] <= 122)
+			s[i] -= 32;
+	}
+	return (s);
+}
