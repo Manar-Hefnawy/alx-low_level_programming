20c956aca8b9d97a88d03b507156a385c9c11cfb
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-19 18:38:04 -0700

    Create 1-strncat.c source code file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings.
+ *
+ * @dest: first string
+ * @src: second string
+ * @n: number of bytes used from src at most.
+ *
+ * Return: concatenated string.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++);
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
+		dest[i + j] = src[j];
+
+	*dest[i + j] = '\0';
+	return (dest);
+}
