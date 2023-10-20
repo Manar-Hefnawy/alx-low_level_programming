baa651c26a264aa83cc4ab35a5adf4c1fa37c985
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-10-19 21:07:15 -0700

    Create 4-rev_array.c source code file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - a function that reverses the content of an array of integers
+ *
+ * @a: array of integers
+ * @n: number of elements to swap
+ */
+
+void reverse_array(int *a, int n)
+{
+	int s, e;
+
+	for (s = 0; s < (n-1) / 2; s++)
+	{
+		e = a[s];
+		a[s] = a[n - 1 - s];
+		a[n - 1 - s] = e;
+	}
+}
