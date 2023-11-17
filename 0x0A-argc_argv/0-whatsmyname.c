3ac287dbf00a99b3c66a7885f06cd3e5f43a2b47
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-11-17 15:28:42 -0800

    Fixing betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,17 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - prints the name of the program
  *
  * @argc: number of commmand line arguments
  * @argv: array containing the command line argument
  *
- * Return: 0 (always success)
+ * Return: always 0 (success)
  */
 
 int main(int argc __attribute__((unused)), char* argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
