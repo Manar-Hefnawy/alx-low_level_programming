c5daa989959e3161f536bedd78ef2ec1561e9973
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-11-17 15:45:05 -0800

    Fix checker

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - a function that prints the number of arguments passed into it.
@@ -10,7 +9,7 @@
  * Return: always 0 (Success)
  */
 
-int main(int argc, char* argv[]__attribute__((unused)))
+int main(int argc, char *argv[]__attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
