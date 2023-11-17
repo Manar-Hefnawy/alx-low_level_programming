80a73bb89e455945c4bdb8aef717c044631030e2
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-11-17 15:18:16 -0800

    Create 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of commmand line arguments
+ * @argv: array containing the command line argument
+ *
+ * Return: 0 (always success)
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
