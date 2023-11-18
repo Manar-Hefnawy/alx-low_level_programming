deb0d559d7b1c6e3814e57ca0960c43d581d5df0
Manar Hefnawy <manarhefnawy793@gmail.com>
2023-11-17 16:24:29 -0800

    Fix compilation error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,7 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0
+ * Return: 0 or 1
  */
 
 int main(int argc, char *argv[])
@@ -19,10 +19,10 @@ int main(int argc, char *argv[])
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
-		return (0);
 	} else if (argc == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }
